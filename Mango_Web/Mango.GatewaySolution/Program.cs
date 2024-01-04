835a3fb33a86cba3fc56e0b89f4981a43d0bf1b0
longntcloudifyvn <tavandai2002@gmail.com>
2024-01-04 21:30:01 +0700

    [Dai] Add API Gateway

---
+++ Mango_Web/Mango.GatewaySolution/Program.cs
@@ -0,0 +1,38 @@
+using Mango.Services.GatewaySolution.Extension;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Authen
+builder.AddAppAuthetication();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Config ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().GetAwaiter().GetResult();
+
+app.Run();
